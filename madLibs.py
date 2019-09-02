f0dd8033357ff7a9798d4e406b300d820cbbb068
Jessica Trinh <jtjessicatrinh@gmail.com>
2019-09-01 17:32:45 -0700

    grammar and design edits

--- madLibs.py
+++ madLibs.py
@@ -14,10 +14,15 @@ def intro():
 
     #ASCII generator http://patorjk.com/
 
-    print('Welcome to MadLibs! MadLibs are short, silly stories based on your words. ')
+    print('Welcome to MadLibs! MadLibs are short, silly stories based on your words. Please follow the rest of the prompt below to start playing! You will be able to read the full story immediately after you complete the prompt.')
     #definition of madLibs: https://www.glowwordbooks.com/blog/category/kids-online-mad-libs/
 
-    print(r'''\
+    #giraffe design credit to @vaultah
+    print(r'''
+                                        Have Fun!
+
+
+                Gerald, the giraffe will be watching you the entire time.
 
                                        ._ o o
                                        \_`-)|_
@@ -28,26 +33,25 @@ def intro():
                             ,"     ## /
                           ,"   ##    /
 
-
                     ''')
 
     print('''
-    \n a proper noun is the name of a person, place or thing: e.g., Alaska
-    \n a noun is a person, place, or thing: e.g. tree
-    \n an adjective is a describing word: e.g., beautiful, warm, cold
-    \n an adverb is word that modifies another word: e.g., gently, quitly
-    \n a verb is a word that describes an action: e.g., hear, run, fly
-    \n a number is an arithmetical value: e.g., 1, 200, 38
+    \n a proper noun is the name of a person, place or thing (e.g., Alaska)
+    \n a noun is a person, place, or thing (e.g. tree)
+    \n an adjective is a describing word (e.g., beautiful, warm, cold)
+    \n an adverb is word that modifies another word (e.g., gently, quitly)
+    \n a verb is a word that describes an action (e.g., hear, run, fly)
+    \n a number is an arithmetical value (e.g., 1, 200, 38)
     ''')
 
 #story 1: random story
 random_story = '''
-    Once upon a time in a town called {} where
+    \n Once upon a time in a town called {},
     \n there was a bird named {},
     \n who felt {} and liked to {}
-    \n {}. Sometime the bird would feel {} and eat {}.
-    \n But when the bird would try to eat {},
-    \n they would see their friend, the {}
+    \n {}. Sometimes the bird would feel {} and eat {}.
+    \n But when the bird would try to eat a {},
+    \n the bird would see their friend, the {}
     \n {}. Together, they would {} see {}
     \n {} for {} hours.
     '''
@@ -60,20 +64,20 @@ def main():
     proper_noun.append(input('Enter another proper noun: '))
 
     noun = []
-    noun.append(input('Enter a noun: '))
-    noun.append(input('Enter a noun: '))
-    noun.append(input('Enter a noun: '))
-    noun.append(input('Enter a noun: '))
+    noun.append(input('Enter a plural noun: '))
+    noun.append(input('Enter a singular noun: '))
+    noun.append(input('Enter another noun: '))
+    noun.append(input('Enter a plural noun: '))
 
     adjective = []
     adjective.append(input('Enter an adjective: '))
-    adjective.append(input('Enter an adjective: '))
-    adjective.append(input('Enter an adjective: '))
-    adjective.append(input('Enter an adjective: '))
+    adjective.append(input('Enter one more adjective: '))
+    adjective.append(input('Enter another adjective: '))
+    adjective.append(input('Enter one last adjective: '))
 
     adverb = []
     adverb.append(input('Enter an adverb: '))
-    adverb.append(input('Enter an adverb: '))
+    adverb.append(input('Enter another adverb: '))
 
     verb = input('Enter a verb: ')
 
